32faa3d3c7b282eba5f3d56750a21902f24ba46c
Tiago de Gregori <sba24297@student.cct.ie>
2024-10-27 14:43:35 +0000

    link to github repo added to main method

--- src/customerdiscountapp/CustomerDiscountApp.java
+++ src/customerdiscountapp/CustomerDiscountApp.java
@@ -1,4 +1,5 @@
 /*
+ * Link to GitHub repo https://github.com/taggrgr/CustomerDiscountApp
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
